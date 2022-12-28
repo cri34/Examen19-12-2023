5228684110534ab7920f135905e0667344d4591b
Cristian Gonzalez Sierra <cristiangs777@gmail.com>
2022-12-28 15:04:15 +0100

    1a complete

--- src/Examen.java
+++ src/Examen.java
@@ -1,3 +1,13 @@
 public class Examen {
-    
+    public boolean isPerfect(int num) {
+        int r = 0;
+        if (num <= 1) {
+            return false;
+        }
+        for (int d = 1; d < num; d++) {
+            if (num % d == 0)
+                r += d;
+        }
+        return r == num;
+    }
 }
--- src/TestExamen.java
+++ src/TestExamen.java
@@ -1,5 +1,7 @@
 public class TestExamen {
     public static void main(String[] args) {
+        Examen e = new Examen();
+        System.out.println(e.isPerfect(2));
 
     }
 }
